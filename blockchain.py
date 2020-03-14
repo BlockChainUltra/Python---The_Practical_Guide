f4d84bd9a49bab12b7ea00f61533e800ed5de646
Mal <blockchain_ultra@protonmail.com>
2020-03-14 18:16:23 +0900

    Python blockchain with for and while loops, and an if/else conditional. This gives us a basic user interface and allows the user to enter multiple transaction amounts.

--- blockchain.py
+++ blockchain.py
@@ -16,17 +16,30 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """returns the input of the user, a new transaction amount - a float"""
     return float(input("Please enter your transaction amount: "))
 
+def get_user_choice():
+    user_input = input("Your choice: ")
+    return user_input
+#tx_amount = get_user_input()
+#add_value(tx_amount, get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+def print_blockchain_elements():
+    """ Output the blochcian to the console"""
+    for block in blockchain:
+            print(block)
 
 
-print(blockchain)
+while True:
+    print("Please choose hat you would like to do:")
+    print("1: Add a new transaction value.")
+    print("2: Output the blockchain blocks.")
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
+        
